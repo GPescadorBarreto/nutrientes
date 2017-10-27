4ff0258212ed898c4123419526b1ca4b89e27370
GPescadorBarreto <alu0100895605@ull.edu.es>
2017-10-27 00:41:20 +0000

    lipids attribute added to food class, not null test passed

--- lib/food.rb
+++ lib/food.rb
@@ -1,8 +1,9 @@
 class Food
-    attr_reader :name, :proteins, :carbs
-    def initialize(name, proteins, carbs)
+    attr_reader :name, :proteins, :carbs, :lipids
+    def initialize(name, proteins, carbs, lipids)
         @name = name
         @proteins = proteins
         @carbs = carbs
+        @lipids = lipids
     end
 end
