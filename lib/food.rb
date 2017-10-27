851daec15b59685acaaa724630551e81bddfc4d5
GPescadorBarreto <alu0100895605@ull.edu.es>
2017-10-27 00:32:29 +0000

    proteins attribute added to food class, not null test passed

--- lib/food.rb
+++ lib/food.rb
@@ -1,6 +1,7 @@
 class Food
-    attr_reader :name
-    def initialize(name)
+    attr_reader :name, :proteins
+    def initialize(name, proteins)
         @name = name
+        @proteins = proteins
     end
 end
