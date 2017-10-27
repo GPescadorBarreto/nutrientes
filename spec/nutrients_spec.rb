cf2e2b9f8594d0fcc3aa0cf558e6cc6c6c6119af
GPescadorBarreto <alu0100895605@ull.edu.es>
2017-10-27 00:30:43 +0000

    Test for not null proteins attribute value added

--- spec/nutrients_spec.rb
+++ spec/nutrients_spec.rb
@@ -7,12 +7,15 @@ RSpec.describe Nutrients do
   describe Food do
     
     before(:all) do
-      @chocolate = Food.new("chocolate")
+      @chocolate = Food.new("chocolate", 5.3)
     end
     
     it "is named" do
       expect(@chocolate.name).not_to be nil
     end
     
+    it "has a proteins value" do
+      expect(@chocolate.proteins).not_to be nil
+    end
   end
 end
