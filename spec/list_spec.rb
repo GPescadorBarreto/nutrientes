ffbcf23eb143f0bb92be1a9d81e3eefd55e1b056
GPescadorBarreto <alu0100895605@ull.edu.es>
2017-11-10 01:01:55 +0000

    Node next & prev test added

--- spec/list_spec.rb
+++ spec/list_spec.rb
@@ -3,10 +3,19 @@ require "spec_helper"
 RSpec.describe Node do
     before(:all) do
        @nodeA = Node.new(0)
+       @nodeB = Node.new(1)
+       @nodeC = Node.new(1)
     end
     
     it "has a value" do
         expect(@nodeA.node[:value]).not_to be nil
+    end
+    
+    it "has a next and a prev" do
+        @nodeA.node[:prev] = @nodeB
+        @nodeA.node[:next] = @nodeC
+        expect(@nodeA.node[:next]).not_to be nil
+        expect(@nodeA.node[:prev]).not_to be nil
     end
 end
 
