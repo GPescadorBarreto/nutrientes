d8106a1b979eeaf9dd7baccde36cffd1d0a6a6f8
GPescadorBarreto <alu0100895605@ull.edu.es>
2017-11-10 01:56:06 +0000

    List class created, Node addition to list by the left added, head and tail test passed

--- lib/list.rb
+++ lib/list.rb
@@ -5,3 +5,18 @@ class Node
         @node = @@node_struct.new(value, nil, nil)
     end
 end
+
+class List
+    attr_reader :head, :tail
+    def initialize()
+        @head = nil
+        @tail = nil
+    end
+   
+    def  put_left(new_node)
+        if (@head == nil && @tail == nil) then
+            @head = new_node
+            @tail = new_node
+        end
+    end
+end
--- lib/nutrients/version.rb
+++ lib/nutrients/version.rb
@@ -1,3 +1,3 @@
 module Nutrients
-  VERSION = "1.0.1"
+  VERSION = "1.0.3"
 end
